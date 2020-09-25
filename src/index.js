26dcc9b9615381b4129522a3fafa7f81a9f02cc9
Marcus Chen <burgesschen1990@gmail.com>
2020-09-24 22:15:24 -0400

    change title and logo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
-ReactDOM.render(<UserForms />, document.getElementById('root'));
+ReactDOM.render(
+
+<UserForms />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
